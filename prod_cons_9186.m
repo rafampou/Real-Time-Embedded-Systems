3714eafea5397765044898bdbfee6316b7fe27aa
rafampou <rafiboul@gmail.com>
2020-03-31 13:22:22 +0300

    main

--- prod_cons_9186.m
+++ prod_cons_9186.m
@@ -1,9 +1,9 @@
-[A,delimiterOut]=importdata("data_9186.txt");
+[A,delimiterOut]=importdata("data64_9186.txt");
 
 B=mean(A,2);
 figure
 hold on;
-title('Line Plot of waiting time on queue')
+title('Line Plot of waiting time on queue with 64 producers')
 ylabel('Waiting Time (ns)') 
 xlabel('Number of Consumer. (Power of 2)') 
 plot(B)
@@ -13,4 +13,4 @@ hold on;
 title('boxplot of waiting time')
 ylabel('Waiting Time (ns)') 
 xlabel('Number of Consumer. (Power of 2)') 
-boxplot(A')
+boxplot(A','whisker',10)
